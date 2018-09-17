df13d14d2ed6ced9f13ae299cd8031f6f3960afb
kanegon <kanegon9365@gmail.com>
2018-09-17 06:28:47 +0000

    initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root "static_pages#home"
+  get "/help",to:"static_pages#help"
+  
+  get 'signup',to:"users#new"
+  post 'signup',to:'users#create'
+  get '/login',to:"users#login"
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
