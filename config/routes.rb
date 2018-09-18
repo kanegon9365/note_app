e8a8d62e9637edca1570bec35fb54eeed71efbec
kanegon <kanegon9365@gmail.com>
2018-09-18 11:51:21 +0000

    day2 commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/help",to:"static_pages#help"
   
-  get 'signup',to:"users#new"
-  post 'signup',to:'users#create'
+  get '/signup',to:"users#new"
   get '/login',to:"users#login"
-  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
